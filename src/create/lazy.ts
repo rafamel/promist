5212948eec183de81d8bb30029067df3270a0a68
Rafa Mel <melrcm@gmx.net>
2019-05-13 14:37:08 +0200

    feat(create/lazy): adds lazy.fn

--- src/create/lazy.ts
+++ src/create/lazy.ts
@@ -1,11 +1,14 @@
 import intercept from '~/helpers/intercept';
+import isPromise from '~/utils/is-promise';
 
 export type TExecutor = (
   resolve: (val?: any) => void,
   reject: (reason: Error) => void
 ) => void;
 
-export default function lazy(executor: TExecutor): Promise<any> {
+export default lazy;
+
+function lazy(executor: TExecutor): Promise<any> {
   const promise: Promise<any> = new Promise((resolve) => resolve());
 
   let p: Promise<any>;
@@ -13,3 +16,15 @@ export default function lazy(executor: TExecutor): Promise<any> {
     return p || (p = new Promise(executor));
   });
 }
+
+lazy.fn = function lazyFn<T>(fn: () => Promise<T> | T): Promise<T> {
+  return lazy((resolve, reject) => {
+    try {
+      const res = fn();
+      if (isPromise(res)) res.then(resolve).catch(reject);
+      else resolve(res);
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
--- test/create/lazy.test.ts
+++ test/create/lazy.test.ts
@@ -1,42 +1,94 @@
 import lazy from '~/create/lazy';
 
-test(`Executor is not run until then is called`, () => {
-  let run = false;
-  const p = lazy((resolve) => (run = true) && resolve());
+describe(`lazy`, () => {
+  test(`Executor is not run until then is called`, () => {
+    let run = false;
+    const p = lazy((resolve) => (run = true) && resolve());
 
-  expect(run).toBe(false);
-  p.then();
-  expect(run).toBe(true);
-});
-test(`Executor is not run until catch is called`, () => {
-  let run = false;
-  const p = lazy((resolve) => (run = true) && resolve());
-
-  expect(run).toBe(false);
-  p.catch();
-  expect(run).toBe(true);
-});
-test(`Resolves`, async () => {
-  const p = lazy((resolve) => resolve(1));
-  await expect(p).resolves.toBe(1);
-});
-test(`Rejects`, async () => {
-  // @ts-ignore
-  const p = lazy((_, reject) => reject(1));
-  await expect(p).rejects.toBe(1);
-});
-test(`Executor doesn't run twice`, async () => {
-  let run = 0;
-  const p = lazy((resolve) => {
-    run++;
-    resolve(1);
+    expect(run).toBe(false);
+    p.then();
+    expect(run).toBe(true);
   });
+  test(`Executor is not run until catch is called`, () => {
+    let run = false;
+    const p = lazy((resolve) => (run = true) && resolve());
 
-  expect(run).toBe(0);
-  p.then();
-  expect(run).toBe(1);
-  await expect(p).resolves.toBe(1);
-  expect(run).toBe(1);
-  p.then();
-  expect(run).toBe(1);
+    expect(run).toBe(false);
+    p.catch();
+    expect(run).toBe(true);
+  });
+  test(`Resolves`, async () => {
+    const p = lazy((resolve) => resolve(1));
+    await expect(p).resolves.toBe(1);
+  });
+  test(`Rejects`, async () => {
+    // @ts-ignore
+    const p = lazy((_, reject) => reject(1));
+    await expect(p).rejects.toBe(1);
+  });
+  test(`Executor doesn't run twice`, async () => {
+    let run = 0;
+    const p = lazy((resolve) => {
+      run++;
+      resolve(1);
+    });
+
+    expect(run).toBe(0);
+    p.then();
+    expect(run).toBe(1);
+    await expect(p).resolves.toBe(1);
+    expect(run).toBe(1);
+    p.then();
+    expect(run).toBe(1);
+  });
+});
+
+describe(`lazy.fn`, () => {
+  test(`doesn't run until then is called`, async () => {
+    const fn = jest.fn().mockImplementation(() => Promise.resolve(10));
+    const promise = lazy.fn(fn);
+    expect(fn).not.toHaveBeenCalled();
+
+    promise.then();
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+  test(`doesn't run until catch is called`, async () => {
+    const fn = jest.fn().mockImplementation(() => Promise.resolve(10));
+    const promise = lazy.fn(fn);
+    expect(fn).not.toHaveBeenCalled();
+
+    promise.catch();
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+  test(`doesn't run twice`, async () => {
+    const fn = jest.fn().mockImplementation(() => Promise.resolve(10));
+    const promise = lazy.fn(fn);
+    expect(fn).not.toHaveBeenCalled();
+
+    await promise;
+    await promise;
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+  test(`resolves`, async () => {
+    const fn1 = jest.fn().mockImplementation(() => Promise.resolve(10));
+    const fn2 = jest.fn().mockImplementation(() => 10);
+    const promise1 = lazy.fn(fn1);
+    const promise2 = lazy.fn(fn2);
+
+    await expect(promise1).resolves.toBe(10);
+    await expect(promise2).resolves.toBe(10);
+  });
+  test(`rejects`, async () => {
+    const fn1 = jest
+      .fn()
+      .mockImplementation(() => Promise.reject(Error('Foo')));
+    const fn2 = jest.fn().mockImplementation(() => {
+      throw Error('Foo');
+    });
+    const promise1 = lazy.fn(fn1);
+    const promise2 = lazy.fn(fn2);
+
+    await expect(promise1).rejects.toThrowErrorMatchingInlineSnapshot(`"Foo"`);
+    await expect(promise2).rejects.toThrowErrorMatchingInlineSnapshot(`"Foo"`);
+  });
 });
