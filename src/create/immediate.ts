0872f713d88e10d4ed8333b24bb205428922c23b
Rafa Mel <melrcm@gmx.net>
2019-10-02 03:08:55 +0200

    feat(create): removes immediate
    BREAKING CHANGE: Removes `immediate` create function, as it's entirely dispensable; it additionally
    relied on non-standard features

--- src/create/immediate.ts
+++
@@ -1,3 +0,0 @@
-export default function immediate(): Promise<void> {
-  return new Promise((resolve) => setImmediate(resolve));
-}
--- src/create/index.ts
+++ src/create/index.ts
@@ -1,5 +1,4 @@
 export { default as deferred } from './deferred';
-export { default as immediate } from './immediate';
 export { default as lazy } from './lazy';
 export { default as wait } from './wait';
 export { default as waitUntil } from './wait-until';
