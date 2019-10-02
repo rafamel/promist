4905d8419d27b4639328e2124e2dba06eeeb5186
Rafa Mel <melrcm@gmx.net>
2019-10-02 02:48:04 +0200

    feat(utils): replaces compose with pipe
    BREAKING CHANGE: The `compose` utility function has been replaced for `pipe` in order to preserve
    type safety

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,3 @@
-export { default as compose } from './compose';
 export { default as control } from './control';
 export { default as isPromise } from './is-promise';
+export { default as pipe } from './pipe';
