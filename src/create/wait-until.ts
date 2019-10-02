24e367e9c1ac23c19a3c1716beef3d8d731f0971
Rafa Mel <melrcm@gmx.net>
2019-10-02 14:40:23 +0200

    refactor(create): simplifies lazy.fn and waitUntil implementations

--- src/create/wait-until.ts
+++ src/create/wait-until.ts
@@ -1,14 +1,18 @@
 import wait from './wait';
 
 export default function waitUntil(
-  testCb: () => boolean | Promise<boolean>,
+  test: () => boolean | Promise<boolean>,
   ms: number = 20
 ): Promise<void> {
-  const callCb = (): Promise<any> => {
-    return Promise.resolve(testCb()).then((ans) => {
-      return ans || wait(ms).then(() => callCb());
-    });
+  const callCb = (): Promise<void> => {
+    try {
+      return Promise.resolve(test()).then((ans) => {
+        return ans ? undefined : wait(ms).then(() => callCb());
+      });
+    } catch (err) {
+      return Promise.reject(err);
+    }
   };
 
-  return callCb().then(() => {});
+  return callCb();
 }
