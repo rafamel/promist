b19294c091531758219ca1792c1cc4b688f00bba
Rafa Mel <melrcm@gmx.net>
2019-10-02 14:45:57 +0200

    feat(create/deferred): guards agains unassigned values for resolve and reject

--- src/create/deferred.ts
+++ src/create/deferred.ts
@@ -1,17 +1,30 @@
 import mark from '~/helpers/mark';
-import { IDeferrable } from '~/types';
+import { IDeferrable, IStateful } from '~/types';
 
 export default function deferred(): IDeferrable & Promise<any> {
-  let _resolve: any;
-  let _reject: any;
+  const state: IStateful = {
+    status: 'pending',
+    value: null,
+    reason: null
+  };
+  let _resolve = (value: any): void => {
+    state.status = 'resolved';
+    state.value = value;
+  };
+  let _reject = (reason: Error): void => {
+    state.status = 'rejected';
+    state.reason = reason;
+  };
 
   const p = new Promise((resolve, reject) => {
+    if (state.status === 'resolved') return resolve(state.value);
+    if (state.status === 'rejected') return reject(state.reason);
     _resolve = resolve;
     _reject = reject;
   }) as IDeferrable & Promise<any>;
 
   mark.set(p, 'deferrable');
-  p.resolve = _resolve;
-  p.reject = _reject;
+  p.resolve = (value: any) => _resolve(value);
+  p.reject = (reason: Error) => _reject(reason);
   return p;
 }
