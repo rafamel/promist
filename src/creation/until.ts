8cc3dd28e191c4870894880e8b5e3d85347fbbac
Rafa Mel <melrcm@gmx.net>
2021-12-13 14:12:52 +0000

    feat(creation): add until

---
+++ src/creation/until.ts
@@ -0,0 +1,47 @@
+import { TypeGuard } from 'type-core';
+import { AbortSignal } from 'abort-controller';
+import { CancellablePromise } from '../classes';
+
+export interface UntilOptions {
+  /** Milliseconds to wait before each `test` execution */
+  delay: number;
+  /** If `true`, `test` errors will not cause rejections */
+  ignoreError?: boolean;
+  /** An AbortSignal to trigger cancellation */
+  abortSignal?: AbortSignal;
+}
+
+/**
+ * Will not resolve until `test` returns `true`.
+ */
+export function until(
+  delay: number | UntilOptions,
+  test: () => boolean | Promise<boolean>
+): CancellablePromise<void> {
+  const opts = TypeGuard.isNumber(delay) ? { delay } : delay;
+
+  return new CancellablePromise<void>((resolve, reject) => {
+    let timeout: NodeJS.Timeout | null = null;
+
+    const reset = (): any => {
+      timeout = setTimeout(trunk, opts.delay || 0);
+    };
+
+    trunk();
+    function trunk(): void {
+      try {
+        Promise.resolve(test()).then(
+          (value) => (value ? resolve() : reset()),
+          (reason) => (opts.ignoreError ? reset() : reject(reason))
+        );
+      } catch (err) {
+        opts.ignoreError ? reset() : reject(err);
+      }
+    }
+
+    return () => {
+      if (timeout) clearTimeout(timeout);
+      resolve();
+    };
+  }, opts.abortSignal);
+}
---
+++ test/creation/until.test.ts
@@ -0,0 +1,95 @@
+import { test, expect } from '@jest/globals';
+import { AbortController } from 'abort-controller';
+import { CancellablePromise } from '../../src/classes';
+import { until } from '../../src/creation';
+
+test(`returns CancellablePromise`, () => {
+  const p = until(0, () => true);
+  expect(p).toBeInstanceOf(CancellablePromise);
+});
+test(`resolves once test returns true`, async () => {
+  let val = false;
+  const p = until(0, () => val);
+
+  const start = Date.now();
+  setTimeout(() => (val = true), 250);
+  await expect(p).resolves.toBe(undefined);
+  expect(Date.now() - start).toBeGreaterThanOrEqual(200);
+  expect(Date.now() - start).toBeLessThan(400);
+});
+test(`resolves once test resolves true`, async () => {
+  let val = false;
+  const p = until(0, () => Promise.resolve(val));
+
+  const start = Date.now();
+  setTimeout(() => (val = true), 250);
+  await expect(p).resolves.toBe(undefined);
+  expect(Date.now() - start).toBeGreaterThanOrEqual(200);
+  expect(Date.now() - start).toBeLessThan(400);
+});
+test(`treats error as false w/ ignoreError`, async () => {
+  let val = false;
+  const p = until({ delay: 0, ignoreError: true }, () => {
+    if (val) return true;
+    else throw Error();
+  });
+
+  const start = Date.now();
+  setTimeout(() => (val = true), 250);
+  await expect(p).resolves.toBe(undefined);
+  expect(Date.now() - start).toBeGreaterThanOrEqual(200);
+  expect(Date.now() - start).toBeLessThan(400);
+});
+test(`treats rejection as false w/ ignoreError`, async () => {
+  let val = false;
+  const p = until({ delay: 0, ignoreError: true }, () => {
+    return val ? Promise.resolve(val) : Promise.reject(Error());
+  });
+
+  const start = Date.now();
+  setTimeout(() => (val = true), 250);
+  await expect(p).resolves.toBe(undefined);
+  expect(Date.now() - start).toBeGreaterThanOrEqual(200);
+  expect(Date.now() - start).toBeLessThan(400);
+});
+test(`rejects on test error`, async () => {
+  const p = until(0, () => {
+    throw Error(`foo`);
+  });
+  await expect(p).rejects.toThrowError('foo');
+});
+test(`rejects on test rejection`, async () => {
+  const p = until(0, () => Promise.reject(Error(`foo`)));
+  await expect(p).rejects.toThrowError('foo');
+});
+test(`uses delay value`, async () => {
+  let val = false;
+  const p = until(500, () => val);
+
+  const start = Date.now();
+  setTimeout(() => (val = true), 200);
+  await expect(p).resolves.toBe(undefined);
+  expect(Date.now() - start).toBeGreaterThanOrEqual(450);
+  expect(Date.now() - start).toBeLessThan(750);
+});
+test(`resolves early on cancel`, async () => {
+  let val = false;
+  const p = until(0, () => val);
+  p.cancel();
+
+  const start = Date.now();
+  setTimeout(() => (val = true), 200);
+  await expect(p).resolves.toBe(undefined);
+  expect(Date.now() - start).toBeLessThan(150);
+});
+test(`resolves early on signal abort`, async () => {
+  let val = false;
+  const controller = new AbortController();
+  const p = until({ delay: 0, abortSignal: controller.signal }, () => val);
+  controller.abort();
+
+  const start = Date.now();
+  setTimeout(() => (val = true), 200);
+  await expect(p).resolves.toBe(undefined);
+  expect(Date.now() - start).toBeLessThan(150);
+});
